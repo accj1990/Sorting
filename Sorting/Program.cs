cea8d611e8bcdaa274d46cd8cfc82217451140d4
anaclars2 <166453698+anaclars2@users.noreply.github.com>
2025-04-06 20:56:16 -0300

    06/04 #03

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -1,4 +1,6 @@
-﻿using Sorting.basic_class.@static;
+﻿using Sorting;
+using Sorting.basic_class.@static;
+using Sorting.manager;
 
 public class Program
 {
@@ -13,11 +15,11 @@ public class Program
             Console.WriteLine("Insira a opção desejada, o número '1' se deseja continuar o programa.\nCaso contrário, digite qualquer outro número inteiro.");
             op = int.Parse(Console.ReadLine());
             Console.WriteLine(" ");
-            if (op == 1) { Menu(); }
+            if (op == 1) { Sorting.Menu.Options(); }
 
         } while (op == 1);
       
-              // Fila, Pilha e Lista em alocação estática
+        // Fila, Pilha e Lista em alocação estática
         Fila f = new Fila(5);
 
         f.Inserir(1);
@@ -62,76 +64,5 @@ public class Program
         p.Remover();
 
         p.Mostrar();
-    }
-
-    private static int[] ChooseFile()
-    {
-        Console.WriteLine("Escolha o arquivo que será lido");
-        Console.WriteLine("A - 10-aleatorios");
-        Console.WriteLine("B - 100-aleatorios");
-        Console.WriteLine("C - 1000-aleatorios");
-        Console.WriteLine("D - 10000-aleatorios");
-        Console.WriteLine("E - 100000-aleatorios");
-        Console.WriteLine("F - 1000000-aleatorios");
-        char op = char.ToUpper(char.Parse(Console.ReadLine()));
-        int[] vet = new int[1];
-
-        Console.WriteLine(" ");
-        switch (op)
-        {
-            case 'A': Console.WriteLine("Opção A: Arquivo '10-aleatorios' escolhido."); vet = new int[10]; vet = ManagerFileReader.File10(); break;
-            case 'B': Console.WriteLine("Opção B: Arquivo '100-aleatorios' escolhido."); vet = new int[100]; vet = ManagerFileReader.File100(); break;
-            case 'C': Console.WriteLine("Opção C: Arquivo '1000-aleatorios' escolhido."); vet = new int[1000]; vet = ManagerFileReader.File1000(); break;
-            case 'D': Console.WriteLine("Opção D: Arquivo '10000-aleatorios' escolhido."); vet = new int[10000]; vet = ManagerFileReader.File10000(); break;
-            case 'E': Console.WriteLine("Opção E: Arquivo '100000-aleatorios' escolhido."); vet = new int[100000]; vet = ManagerFileReader.File100000(); break;
-            case 'F': Console.WriteLine("Opção F: Arquivo '1000000-aleatorios' escolhido."); vet = new int[1000000]; vet = ManagerFileReader.File1000000(); break;
-        }
-        Console.WriteLine(" "); return vet;
-    }
-    private static Sorting.enums.Sortings ChooseAlgorithm(int[] vet)
-    {
-        Console.WriteLine("Escolha o algoritmo que será executado");
-        Console.WriteLine("A - Bubble Sort");
-        Console.WriteLine("B - Selection Sort");
-        Console.WriteLine("C - Insertion Sort");
-        Console.WriteLine("D - Bucket Sort");
-        Console.WriteLine("E - Counting Sort");
-        Console.WriteLine("F - Radix Sort");
-        Console.WriteLine("G - Shell Sort");
-        Console.WriteLine("H - Quick Sort");
-        Console.WriteLine("I - Merge Sort");
-        Console.WriteLine("J - Heap Sort");
-        char op = char.ToUpper(char.Parse(Console.ReadLine()));
-        Sorting.enums.Sortings algorithm = Sorting.enums.Sortings.BUBBLESORT;
-
-        Console.WriteLine(" ");
-        switch (op)
-        {
-            case 'A': Console.WriteLine("Opção A: Algoritmo 'Bubble Sort' escolhido."); algorithm = Sorting.enums.Sortings.BUBBLESORT; break;
-            case 'B': Console.WriteLine("Opção B: Algoritmo 'Selection Sort' escolhido."); algorithm = Sorting.enums.Sortings.SELECTIONSORT; break;
-            case 'C': Console.WriteLine("Opção C: Algoritmo 'Insertion Sort' escolhido."); algorithm = Sorting.enums.Sortings.INSERTIONSORT; break;
-            case 'D': Console.WriteLine("Opção D: Algoritmo 'Bucket Sort' escolhido."); algorithm = Sorting.enums.Sortings.BUCKETSORT; break;
-            case 'E': Console.WriteLine("Opção E: Algoritmo 'Counting Sort' escolhido."); algorithm = Sorting.enums.Sortings.COUNTINGSORT; break;
-            case 'F': Console.WriteLine("Opção F: Algoritmo 'Radix Sort' escolhido."); algorithm = Sorting.enums.Sortings.RADIXSORT; break;
-            case 'G': Console.WriteLine("Opção G: Algoritmo 'Shell Sort' escolhido."); algorithm = Sorting.enums.Sortings.SHELLSORT; break;
-            case 'H': Console.WriteLine("Opção H: Algoritmo 'Quick Sort' escolhido."); algorithm = Sorting.enums.Sortings.QUICKSORT; break;
-            case 'I': Console.WriteLine("Opção I: Algoritmo 'Merge Sort' escolhido."); algorithm = Sorting.enums.Sortings.MERGESORT; break;
-            case 'J': Console.WriteLine("Opção J: Algoritmo 'Heap Sort' escolhido."); algorithm = Sorting.enums.Sortings.HEAPSORT; break;
-        }
-        Console.WriteLine(" "); return algorithm;
-    }
-
-    private static void Menu() // solicitando ao usuario algoritmo e arquivo desejados
-    {
-        int[] vet = ChooseFile();
-        Sorting.enums.Sortings algorithm = ChooseAlgorithm(vet);
-
-        /* Console.WriteLine("Vetor antes da ordenação:");
-          PrintSolutionStatic.ArraySameLine(algorithm, vet); */
-        ManagerFileSorting.Order(algorithm, vet);
-
-        /* Console.WriteLine("\nVetor depois da ordenação:");
-        PrintSolutionStatic.ArraySameLine(algorithm, vet); */
-        Console.WriteLine(" ");
     }
 }
